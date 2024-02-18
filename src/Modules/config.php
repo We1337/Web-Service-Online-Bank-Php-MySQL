5a5a832c9a3badea00fc3525b610326ad1183b69
We1337 <weuser149@gmail.com>
2024-02-18 16:41:19 +0600

    feat: connection database created

---
+++ src/Modules/config.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("db_config.php");
+
+try {
+    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: ". $e->getMessage();
+}
+// $conn = null; connection closed
---
+++ src/Modules/db_config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "bank");
+define("DB_USER","We1337");
+define("DB_PASSWORD","password");
+define("DB_CHARSET","utf8");
