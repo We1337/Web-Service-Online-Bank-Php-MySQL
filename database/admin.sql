b7c68ab90a8c61d9c4aab22af249df0fea258b69
We1337 <weuser149@gmail.com>
2024-02-19 01:27:54 +0600

    refactor: admin table structured and updated with new data

---
+++ database/admin.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `Admins` (
+    `AdminID` INT PRIMARY KEY AUTO_INCREMENT,
+    `Username` VARCHAR(255) UNIQUE NOT NULL,
+    `Password` VARCHAR(255) NOT NULL,
+    `FirstName` VARCHAR(50) NOT NULL,
+    `LastName` VARCHAR(50) NOT NULL,
+    `Email` VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Insert fake data
+INSERT INTO `Admins` (`Username`, `Password`, `FirstName`, `LastName`, `Email`)
+VALUES
+    ('admin', '$2y$10$iwVJovXdgi1Ggc3Y80y/Z.LQa13pP6y.lgoxTDQP50CAfcSg.QRGG', 'John', 'Doe', 'admin1@example.com');
