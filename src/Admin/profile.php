094a76c2ee2e8297ab98034ac21886bcf7042518
We1337 <weuser149@gmail.com>
2024-02-19 14:05:20 +0600

    feat: display message from update profile

--- src/Admin/profile.php
+++ src/Admin/profile.php
@@ -10,7 +10,7 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
-<div class="container mt-5" style="padding-bottom: 7rem;">
+<div class="container mt-5" style="padding-bottom: 10rem;">
     <h2>Edit Profile</h2>
     
     <form action="module/profile_update.php" method="post">
@@ -30,33 +30,63 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         <!-- Email Input -->
         <div class="form-group" style="padding-top: 1rem;">
+            <?php
+            if(isset($_SESSION["message"]["email"])) {
+                echo '<div class="alert alert-warning" role="alert">Email</div>';
+            }
+            ?>
             <label for="email">Email</label>
             <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="<?php echo $row['Email']; ?>" required>
         </div>
 
         <div class="form-group" style="padding-top: 1rem;">
+            <?php 
+            if(isset($_SESSION["message"]["firstname"])) {
+                echo '<div class="alert alert-warning" role="alert">Firstname</div>';
+            }
+            ?>
             <label for="firstname">First name</label> 
             <input type="firstname" class="form-control" id="firstname" name="firstname" placeholder="First name" value="<?php echo $row['FirstName'] ?>" require>
         </div>
 
         <div class="form-group" style="padding-top: 1rem;">
+            <?php
+            if(isset($_SESSION["message"]["lastname"])) {
+                echo '<div class="alert" alert-warning" role="alert">Lastname</div>';
+            }
+            ?>
             <lable for="lastname">Last name</lable> 
             <input type="lastname" class="form-control" id="lastname" name="lastname" placeholder="Last name" value="<?php echo $row['LastName']; ?>" require>
         </div>
 
         <div class="form-group" style="padding-top: 1rem;">
+            <?php
+            if(isset($_SESSION["message"]["oldpassword"])) {
+                echo '<div class="alert alert-warning" role="alert">Old password</div>';
+            }
+            ?>
             <label for="lastname">Old Password</label>
             <input type="lastname" class="form-control" id="oldpassword" name="oldpassword" placeholder="Enter old password">
         </div>
 
         <!-- Password Input -->
         <div class="form-group" style="padding-top: 1rem;">
+            <?php
+            if(isset($_SESSION["message"]["password"])) {
+                echo '<div class="alert alert-warning" role="alert">Password</div>';
+            }
+            ?>
             <label for="password">Password</label>
             <input type="password" class="form-control" id="password" name="password" placeholder="Enter new password">
         </div>
 
         <!-- Confirm Password Input -->
         <div class="form-group" style="padding-top: 1rem;">
+            <?php
+            if(isset($_SESSION["message"]["confirmpassword"])) {
+                echo '<div class="alert alert-warning" role="alert">Confirm password</div>';
+            }
+            ?>
             <label for="confirmPassword">Confirm Password</label>
             <input type="password" class="form-control" id="confirmpassword" name="confirmpassword" placeholder="Confirm new password">
         </div>
