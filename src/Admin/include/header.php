373f060c78de80bbb89d8953de37b094a23cb4ba
We1337 <weuser149@gmail.com>
2024-02-19 13:48:58 +0600

    feat: header page for admin

---
+++ src/Admin/include/header.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+
+if (isset($_SESSION['admin_session'])) {
+    $username = $_SESSION['admin_username'];
+    $message = $_SESSION['message'];
+} else {
+    $_SESSION['message'] = "Please login";
+    header("Location: login.php");
+}
+?>
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- Chart.js library -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary" style="padding: 1rem;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><b>BankX</b></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Customer
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">New customer</a></li>
+                        <li><a class="dropdown-item" href="#">View customers</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Support
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">New support</a></li>
+                        <li><a class="dropdown-item" href="#">View supprots</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Server
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Backup server</a></li>
+                        <li><a class="dropdown-item" href="#">Shell</a></li>
+                        <li><a class="dropdown-item" href="#">Email</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="profile.php">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Settings</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="include/logout.php">Logout</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
+
+<?php
+
+if (isset($_SESSION['message'])) {
+    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>";
+    echo $_SESSION['message'];
+    echo "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
+    echo "</div>";
+    //unset($_SESSION['message']);
+}
+
+?>
