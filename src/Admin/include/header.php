888fded1ab3c1e8baa213bb98975d652bf3adefc
We1337 <weuser149@gmail.com>
2024-03-03 14:09:21 -0800

    refactor: header link update

--- src/Admin/include/header.php
+++ src/Admin/include/header.php
@@ -31,14 +31,14 @@ if (isset($_SESSION['admin']['session']) && $_SESSION['admin']['session'] === tr
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary" style="padding: 1rem;">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"><b>BankX</b></a>
+        <a class="navbar-brand" href="dashboard.php"><b>BankX</b></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link active" aria-current="page" href="dashboard.php">Home</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
