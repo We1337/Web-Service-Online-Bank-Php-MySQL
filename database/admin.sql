9c75b36a20cf2a66bc0b2e6c4dde4fe296b9c18b
We1337 <weuser149@gmail.com>
2024-03-03 11:04:32 -0800

    feat: login and logout time added

--- database/admin.sql
+++ database/admin.sql
@@ -5,11 +5,13 @@ CREATE TABLE `Admins` (
     `FirstName` VARCHAR(50) NOT NULL,
     `LastName` VARCHAR(50) NOT NULL,
     `Email` VARCHAR(100) UNIQUE NOT NULL,
-    `LoginTime` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `LogoutTime` TIMESTAMP DEFAULT NULL
+    `LoginTime` TIMESTAMP DEFAULT NULL,
+    `LogoutTime` TIMESTAMP DEFAULT NULL 
 );
 
 -- Insert fake data
-INSERT INTO `Admins` (`Username`, `Password`, `FirstName`, `LastName`, `Email`)
+-- username: admin
+-- password: password
+INSERT INTO `Admins` (`Username`, `Password`, `FirstName`, `LastName`, `Email`,  `LoginTime`, `LogoutTime`)
 VALUES
-    ('admin', '$2y$10$iwVJovXdgi1Ggc3Y80y/Z.LQa13pP6y.lgoxTDQP50CAfcSg.QRGG', 'John', 'Doe', 'admin1@example.com');
+    ('admin', '$2y$10$iwVJovXdgi1Ggc3Y80y/Z.LQa13pP6y.lgoxTDQP50CAfcSg.QRGG', 'John', 'Doe', 'admin1@example.com', '2024-03-03 12:00:00', '2024-03-03 18:30:00');
