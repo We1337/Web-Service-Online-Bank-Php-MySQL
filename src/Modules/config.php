f500cb28c1cdc755edac33a18e9c08549ff32bdf
We1337 <weuser149@gmail.com>
2024-03-03 11:45:50 -0800

    feat: display alert message update

--- src/Modules/config.php
+++ src/Modules/config.php
@@ -7,6 +7,6 @@ try {
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Connection failed: ". $e->getMessage();
+    echo "<div class='alert alert-info' role='alert'>Connection failed: " . $e->getMessage() . "</div>";
 }
 // $conn = null; connection closed
