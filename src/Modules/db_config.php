2d85fb9fef3420191879da9df4e1baf0359bd27d
We1337 <weuser149@gmail.com>
2024-03-08 08:52:17 -0800

    refactor: credintials changed

--- src/Modules/db_config.php
+++ src/Modules/db_config.php
@@ -2,6 +2,7 @@
 
 define("DB_HOST", "localhost");
 define("DB_NAME", "bank");
-define("DB_USER","root");
-define("DB_PASSWORD","");
+define("DB_USER","We1337");
+define("DB_PASSWORD","password");
 define("DB_CHARSET","utf8");
+define("DB_PORT", 3300);
