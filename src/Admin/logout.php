1dde72217caf2515a802ad9359f133d5e6272290
We1337 <weuser149@gmail.com>
2024-03-10 04:34:05 -0700

    style: docs updated and commented

--- src/Admin/logout.php
+++ src/Admin/logout.php
@@ -1,20 +1,29 @@
 <?php
-
+// Start the session to access session variables
 session_start();
 
+// Include the configuration file
 require_once("../Modules/config.php");
 
+// Get the username from the session
 $username = $_SESSION['admin']['username'];
 
+// Prepare and execute the query to update the logout time for the admin
 $time = $conn->prepare('UPDATE `Admins` SET `LogoutTime` = CURRENT_TIMESTAMP WHERE `Username` = :username');
 $time->bindParam(':username', $username, PDO::PARAM_STR);
 $time->execute();
-$conn=null;
 
+// Close the database connection
+$conn = null;
+
+// Clear all session variables
 $_SESSION = array();
 
+// Destroy the session
 session_destroy();
 
+// Redirect to the login page
 header("Location: login.php");
 
+// Ensure that no further code is executed after redirection
 exit();
