ba669cab73b9ba7f5e59dff8bc4aace420012599
We1337 <weuser149@gmail.com>
2024-03-10 04:35:35 -0700

    style: documentation updated and commented

--- src/Modules/config.php
+++ src/Modules/config.php
@@ -1,12 +1,16 @@
 <?php
 
+// Include the database configuration file
 require_once("db_config.php");
 
 try {
-    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD);
+    // Create a new PDO connection using the provided database credentials
+    $conn = new PDO("mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD);
 
+    // Set PDO attributes to enable error handling
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+    // Handle any exceptions that might occur during the connection setup
+    // Print an alert message with the error details if the connection fails
     echo "<div class='alert alert-info' role='alert'>Connection failed: " . $e->getMessage() . "</div>";
 }
-// $conn = null; connection closed
