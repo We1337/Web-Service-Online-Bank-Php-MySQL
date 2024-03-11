e8e1817552adb0ab36add960149bdb4956019b4e
We1337 <weuser149@gmail.com>
2024-03-11 01:10:32 -0700

    feat: alert messages added

--- src/Admin/admin_profile.php
+++ src/Admin/admin_profile.php
@@ -6,95 +6,94 @@ require_once("../Modules/config.php");
 $stmt = $conn->prepare('SELECT * FROM `Admins`');
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-if(isset($_SESSION["message"]["admin_data_updated"])) {
-    echo '<div class="alert alert-warning" role="alert"><?php echo $_SESSION["message"]; ?></div>';
-}
-
 ?>
 
 <div class="container mt-5" style="padding-bottom: 10rem;">
     <h2>Edit Profile</h2>
     
-    <form action="admin_profile_update.php" method="post">
+    <form action="admin_profile_update.php" method="POST">
         <?php 
         foreach($result as $row) {
         ?>
-        <!-- Username Input -->
+
         <div class="form-group">
             <?php 
             if(isset($_SESSION["message"]["username"])) {
-                echo '<div class="alert alert-warning" role="alert">This is a warning alert—check it out!</div>';
+                echo '<div class="alert alert-warning" role="alert">Please enter username</div>';
             }
             ?>
             <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" name="username" placeholder="User name" value="<?php echo $row['Username']; ?>">
+            <input type="text" class="form-control" id="username" name="username" placeholder="User name" value="<?= $row['Username']; ?>">
         </div>
 
-        <!-- Email Input -->
         <div class="form-group" style="padding-top: 1rem;">
             <?php
             if(isset($_SESSION["message"]["email"])) {
-                echo '<div class="alert alert-warning" role="alert">Email</div>';
+                echo '<div class="alert alert-warning" role="alert">Please enter email</div>';
             }
             ?>
             <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="<?php echo $row['Email']; ?>" required>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="<?= $row['Email']; ?>" required>
         </div>
 
         <div class="form-group" style="padding-top: 1rem;">
             <?php 
             if(isset($_SESSION["message"]["firstname"])) {
-                echo '<div class="alert alert-warning" role="alert">Firstname</div>';
+                echo '<div class="alert alert-warning" role="alert">Please enter firstname</div>';
             }
             ?>
             <label for="firstname">First name</label> 
-            <input type="firstname" class="form-control" id="firstname" name="firstname" placeholder="First name" value="<?php echo $row['FirstName'] ?>" require>
+            <input type="firstname" class="form-control" id="firstname" name="firstname" placeholder="First name" value="<?= $row['FirstName'] ?>" required>
         </div>
 
         <div class="form-group" style="padding-top: 1rem;">
             <?php
             if(isset($_SESSION["message"]["lastname"])) {
-                echo '<div class="alert alert-warning" role="alert">Lastname</div>';
+                echo '<div class="alert alert-warning" role="alert">Please enter lastname</div>';
             }
             ?>
             <lable for="lastname">Last name</lable> 
-            <input type="lastname" class="form-control" id="lastname" name="lastname" placeholder="Last name" value="<?php echo $row['LastName']; ?>" require>
+            <input type="lastname" class="form-control" id="lastname" name="lastname" placeholder="Last name" value="<?= $row['LastName']; ?>" required>
         </div>
 
         <div class="form-group" style="padding-top: 1rem;">
             <?php
             if(isset($_SESSION["message"]["oldpassword"])) {
-                echo '<div class="alert alert-warning" role="alert">Old password</div>';
+                echo '<div class="alert alert-warning" role="alert">Please enter old password</div>';
+            } else if(isset($_SESSION['message']['password_verification'])) {
+                echo '<div class="alert alert-warning" role="alert">Old password is wrong</div>';
             }
             ?>
-            <label for="lastname">Old Password</label>
-            <input type="password" class="form-control" id="oldpassword" name="oldpassword" placeholder="Enter old password">
+            <label for="oldpassword">Old Password</label>
+            <input type="password" class="form-control" id="oldpassword" name="oldpassword" placeholder="Enter old password" required>
         </div>
 
-        <!-- Password Input -->
         <div class="form-group" style="padding-top: 1rem;">
             <?php
             if(isset($_SESSION["message"]["password"])) {
                 echo '<div class="alert alert-warning" role="alert">Password</div>';
+            } else if(isset($_SESSION['message']['password_is_not_same'])) {
+                echo '<div class="alert alert-warning" role="alert">Password it is not the same</div>';
+            } else if(isset($_SESSION['message']['password_lenght'])) {
+                echo '<div class="alert alert-warning" role="alert">Password length must be 8 characters</div>';
             }
             ?>
             <label for="password">New Password</label>
-            <input type="password" class="form-control" id="password" name="password" placeholder="Enter new password">
+            <input type="password" class="form-control" id="password" name="password" placeholder="Enter new password" required>
         </div>
 
-        <!-- Confirm Password Input -->
         <div class="form-group" style="padding-top: 1rem;">
             <?php
             if(isset($_SESSION["message"]["confirmpassword"])) {
                 echo '<div class="alert alert-warning" role="alert">Confirm password</div>';
+            } else if(isset($_SESSION['message']['password_is_not_same'])) {
+                echo '<div class="alert alert-warning" role="alert">Password it is not the same</div>';
             }
             ?>
-            <label for="confirmPassword">Confirm Password</label>
-            <input type="password" class="form-control" id="confirmpassword" name="confirmpassword" placeholder="Confirm new password">
+            <label for="confirmpassword">Confirm Password</label>
+            <input type="password" class="form-control" id="confirmpassword" name="confirmpassword" placeholder="Confirm new password" required>
         </div>
 
-        <!-- Update Button -->
         <div style="padding-top: 1rem;">
             <button type="submit" class="btn btn-primary">Update Profile</button>
         </div>
