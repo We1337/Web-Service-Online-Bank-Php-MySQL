592490974c5661b8dea0ea1bed22608201d6cf7a
We1337 <weuser149@gmail.com>
2024-03-11 01:07:45 -0700

    feat: mail service added for credentails

--- src/Modules/db_config.php
+++ src/Modules/db_config.php
@@ -6,3 +6,8 @@ define("DB_USER","We1337");
 define("DB_PASSWORD","password");
 define("DB_CHARSET","utf8");
 define("DB_PORT", 3300);
+
+define("MAIL_HOST", "localhost");
+define("MAIL_NAME","user");
+define("MAIL_PASSWORD", "password");
+define("MAIL_PORT","587");
